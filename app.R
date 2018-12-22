e8dc211ea5614f0a300c870c393b189d242ae76a
BitcoinBCH <43348318+BitcoinBCH@users.noreply.github.com>
2018-12-22 23:55:52 +0530

    Add files via upload

--- app.R
+++ app.R
@@ -4,35 +4,6 @@
 #11810099 Gaurav Borse
 #11810036 Shariq Imam
 
-if(!require(shiny)){install.packages("shiny")}
-if(!require(shinydashboard)){install.packages("shinydashboard")}
-if(!require(shinythemes)){install.packages("shinythemes")}
-if(!require(udpipe)){install.packages("udpipe")}
-if(!require(textrank)){install.packages("textrank")}
-if(!require(lattice)){install.packages("lattice")}
-if(!require(igraph)){install.packages("igraph")}
-if(!require(ggraph)){install.packages("ggraph")}
-if(!require(ggplot2)){install.packages("ggplot2")}
-if(!require(wordcloud)){install.packages("wordcloud")}
-if(!require(stringr)){install.packages("stringr")}
-if (!require(extrafont)){install.packages("extrafont")} 
-if (!require(extrafontdb)){install.packages("extrafontdb")}
-library(extrafont)
-library(extrafontdb)
-loadfonts(device = "win")
-#font_import()
-library(shiny)
-library(shinydashboard)
-library(shinythemes)
-library(udpipe)
-library(textrank)
-library(lattice)
-library(igraph)
-library(ggraph)
-library(ggplot2)
-library(wordcloud)
-library(stringr)
-
 options(shiny.maxRequestSize=30*1024^2)
 
 # Different language models 
